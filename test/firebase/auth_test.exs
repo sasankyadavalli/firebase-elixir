8c1ace8dbcc3369aca469bd04aa717d13595a575
Sasank Yadavalli <yadavallisasank@gmail.com>
2017-08-30 02:44:07 +0530

    Add test to signup_with_email/2 to Firebase.AuthTest

---
+++ test/firebase/auth_test.exs
@@ -0,0 +1,18 @@
+defmodule Firebase.AuthTest do
+  use ExUnit.Case
+  
+  test "verify signup_with_email" do
+    email = "yadavallisassank@gmail.com"
+    password = "Sasi@$123456"
+
+    {:ok, %HTTPoison.Response{body: body, headers: _,request_url: _, status_code: status_code}} = Firebase.Auth.signup_with_email(email, password)
+
+    resp_email = 
+      body
+      |> Poison.decode!()
+      |> Map.get("email") 
+    
+    assert status_code == 200
+    assert resp_email == email
+  end
+end
